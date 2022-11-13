ded3b84d03c732cc328f0e37d345f0f012238bcd
YASMINE Chachy <yasmine.chachy15@gmail.com>
2022-11-13 12:41:25 +0100

    Ajoutez des fichiers projet.

---
+++ PHILA/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PHILA.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace PHILA.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ PHILA/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using PHILA.Models;
+using PHILA.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PHILA.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
